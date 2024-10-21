9cc22134e731e69646e72b8ce0d0013e6147b64d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-21 16:38:27 +0400

    feat: add test for mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,63 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
 
-describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+describe('throttledGetDataFromApi', (): void => {
+  beforeAll(() => 
+    jest.useFakeTimers()
+  );
+  
+  beforeEach(() => 
+    jest.runOnlyPendingTimers()
+  );
+  
+  afterEach(() => 
+    jest.clearAllMocks()
+  );
+  
+  afterAll(() => 
+    jest.useRealTimers()
+  );
+
+  const baseURL = 'https://jsonplaceholder.typicode.com';
+
+  const endPoint = '/posts/1';
+
+  test('should create instance with provided base url', async (): Promise<void> => {
+    const spyOnCreateInstance: jest.SpyInstance<AxiosInstance> = jest.spyOn(
+      axios,
+      'create',
+    );
+
+    await throttledGetDataFromApi(endPoint);
+
+    expect(spyOnCreateInstance).lastCalledWith({ baseURL });
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  test('should perform request to correct provided url', async (): Promise<void> => {
+    const spyGet: jest.SpyInstance = jest.spyOn(axios.Axios.prototype, 'get');
+
+    await throttledGetDataFromApi(endPoint);
+
+    expect(spyGet).lastCalledWith(endPoint);
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test('should return response data', async (): Promise<void> => {
+    type Post = {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+    };
+
+    const responseData: Post = {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    };
+
+    const data = await throttledGetDataFromApi(endPoint);
+
+    expect(responseData).toEqual(expect.objectContaining(data));
   });
 });
